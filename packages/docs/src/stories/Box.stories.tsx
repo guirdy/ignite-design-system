9c5c3ed3b87dc46a4d93a367c0b0cdb8b54b3b36
guirdy <guilherme.gl1997@gmail.com>
2023-04-10 15:23:18 -0300

    feat: text component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps } from '@ignite-ui/react/src'
+import { Box, BoxProps, Text } from '@ignite-ui/react/src'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento Box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento Box</Text>,
   },
 } as Meta<BoxProps>
 
